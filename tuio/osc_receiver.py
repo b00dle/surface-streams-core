80406c89654887d1bc3a3ec9344911f41c6bc77f
b00dle <sebastian.stickert@gmail.com>
2019-03-29 16:38:44 +0100

    added osc_receiver as standalone file

---
+++ tuio/osc_receiver.py
@@ -0,0 +1,16 @@
+import multiprocessing
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+class OscReceiver(object):
+    def __init__(self, ip, port, osc_disp=dispatcher.Dispatcher()):
+        self._server = osc_server.ThreadingOSCUDPServer((ip, port), osc_disp)
+        self._server_process = multiprocessing.Process(target=self._server.serve_forever)
+
+    def start(self):
+        print("Serving OSC on {}".format(self._server.server_address))
+        self._server_process.start()
+
+    def terminate(self):
+        print("Stopped Serving OSC on {}".format(self._server.server_address))
+        self._server_process.terminate()
--- tuio/tuio_receiver.py
+++ tuio/tuio_receiver.py
@@ -4,21 +4,8 @@ import multiprocessing
 from pythonosc import dispatcher
 from pythonosc import osc_server
 from tuio.tuio_elements import TuioImagePattern, TuioBounds, TuioSymbol, TuioPointer, TuioData
+from tuio.osc_receiver import OscReceiver
 from typing import Dict
-
-
-class OscReceiver(object):
-    def __init__(self, ip, port, osc_disp=dispatcher.Dispatcher()):
-        self._server = osc_server.ThreadingOSCUDPServer((ip, port), osc_disp)
-        self._server_process = multiprocessing.Process(target=self._server.serve_forever)
-
-    def start(self):
-        print("Serving OSC on {}".format(self._server.server_address))
-        self._server_process.start()
-
-    def terminate(self):
-        print("Stopped Serving OSC on {}".format(self._server.server_address))
-        self._server_process.terminate()
 
 
 def bnd_handler(path, fixed_args, s_id, u_id, x_pos, y_pos, angle, width, height):
