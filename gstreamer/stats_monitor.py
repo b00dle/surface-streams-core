b51371ead36e249fd6e07ec1f63d3fe138a95fdb
basti_circa <sebastian.stickert@gmail.com>
2019-04-13 14:11:51 +0200

    added fps counter to stats monitoring

--- gstreamer/stats_monitor.py
+++ gstreamer/stats_monitor.py
@@ -1,9 +1,9 @@
-from gi.repository import GObject
+from gi.repository import GObject, Gst
 
 
 class UdpStatsMonitor(object):
     """
-    Class monitoring bytes sent from a udpsink element this instance links to.
+    Class monitoring bytes & frames sent from a udpsink element this instance links to.
     Produces console logs for monitored data. Can also store data to csv file.
     """
 
@@ -16,6 +16,8 @@ class UdpStatsMonitor(object):
         self._enabled = False
         self._pipeline = None
         self._element_name = None
+        self._pad_probe_id = None
+        self._num_buffers = 0
         self.file = file
 
     def link(self, pipeline, element_name):
@@ -61,6 +63,21 @@ class UdpStatsMonitor(object):
         if not self._enabled:
             return
         self._enabled = False
+        if self._pad_probe_id is not None:
+            element = self._pipeline.get_by_name(self._element_name)
+            element.get_static_pad("sink").remove_probe(self._pad_probe_id)
+            self._pad_probe_id = None
+            self._num_buffers = 0
+
+    def _increment_buffer_count(self, pad, buf):
+        """
+        This function increments the monitored count of buffers probed
+        :param pad: gst pad probed
+        :param buf: buffer retrieved from probe
+        :return:
+        """
+        self._num_buffers += 1
+        return True
 
     def _update(self):
         """
@@ -71,11 +88,13 @@ class UdpStatsMonitor(object):
         if new_stats is None or not self._enabled:
             return
         bytes_per_second = new_stats["bytes-sent"] - self.stats["bytes-sent"]
+        frames_per_second = new_stats["frames-sent"] - self.stats["frames-sent"]
         if bytes_per_second > 0:
             tag = self._pipeline.get_name() + "." + self._element_name
             print("> " + tag + " stats:", new_stats)
             print("  > bytes/s:", bytes_per_second)
             print("  > mbit/s:", (bytes_per_second * 8) / 1000000)
+            print("  > fps:", frames_per_second)
             with open(self.file, "a") as log_file:
                 log_file.write(tag + "," + str(bytes_per_second) + "\n")
                 log_file.close()
@@ -97,8 +116,11 @@ class UdpStatsMonitor(object):
             element.get_property("host"),
             element.get_property("port")
         )
+        if self._pad_probe_id is None:
+            self._pad_probe_id = element.get_static_pad("sink").add_probe(Gst.PadProbeType.PUSH, self._increment_buffer_count)
         stats = {
             data.nth_field_name(n): data.get_value(data.nth_field_name(n))
             for n in range(0, data.n_fields())
         }
+        stats["frames-sent"] = self._num_buffers
         return stats
